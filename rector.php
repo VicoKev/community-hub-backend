bd25519826f04b54b7232be7eb06efc7aa3c897c
VicoKev <kevinpatinvoh@gmail.com>
2026-03-17 01:30:47 +0100

    feat: add rector configuration for code quality and type declarations

---
+++ rector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/tests',
+    ])
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        codingStyle: true,
+    )
+    ->withPhpSets();
